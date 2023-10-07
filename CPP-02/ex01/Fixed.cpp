a6f29fb16bbd3c4dbaf844f3b1f7966c3e263ea4
Alexis Abda <alexis.abda@hotmail.com>
2023-10-07 19:08:55 +0200

    add CPP-02/ex01/

---
+++ CPP-02/ex01/Fixed.cpp
@@ -0,0 +1,57 @@
+#include "./Fixed.hpp"
+
+int const	Fixed::_fractionaryBits = 8;
+
+Fixed::Fixed(void) : _value(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &a) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = a;
+}
+
+Fixed::Fixed(int const value) : _value(value * (1 << _fractionaryBits)) {
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(float const value) : _value((roundf(value * (1 << _fractionaryBits)))) {
+	std::cout << "Float constructor called" << std::endl;
+
+}
+
+Fixed::~Fixed(void) {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(Fixed const & dest) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &dest)
+	this->_value = dest._value;
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_value);
+}
+
+void	Fixed::setRawBits(int const raw) {
+	this->_value = raw;
+}
+
+float	Fixed::toFloat(void) const {
+	float	res;
+
+	res = (float)this->_value / (1 << this->_fractionaryBits);
+	return (res);
+}
+
+int	Fixed::toInt(void) const {
+	return (this->_value >> this->_fractionaryBits);
+}
+
+std::ostream&	operator<<(std::ostream& output, Fixed const& fixed) {
+	output << fixed.toFloat();
+	return (output);
+}
---
+++ CPP-02/ex01/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+	private:
+		int					_value;
+		static int const	_fractionaryBits;
+
+	public:
+		Fixed(void);
+		Fixed(Fixed const &a);
+		Fixed(int const value);
+		Fixed(float const value);
+		~Fixed(void);
+		Fixed&	operator=(Fixed const & dest);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+};
+
+std::ostream&	operator<<(std::ostream& output, Fixed const& fixed);
+
+#endif
