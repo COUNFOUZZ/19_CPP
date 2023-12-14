da8758dcd8805b9d2163225c1c067852b45f06fc
Alexis Abda <aabda@student.s19.be>
2023-12-14 16:30:25 +0100

    Better logic for function "randomChump" (zombie in the stack and not in the heap)

--- CPP-01/ex00/Zombie.cpp
+++ CPP-01/ex00/Zombie.cpp
@@ -8,6 +8,6 @@ Zombie::~Zombie(void) {
 }
 
 void	Zombie::announce(void) const {
-	std::cout << this->_name << " : BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 	return;
 }
--- CPP-01/ex00/main.cpp
+++ CPP-01/ex00/main.cpp
@@ -6,7 +6,7 @@ int	main(void) {
 
 	stack_zombie.announce();
 	heap_zombie->announce();
-	randomChump("RandomChump_Heap");
+	randomChump("RandomChump_Stack_In_Function");
 	delete heap_zombie;
 	return (0);
 }
