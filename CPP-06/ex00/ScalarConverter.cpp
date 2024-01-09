e17b3c4eca693e58a6b811d43834ee8c9fdcdf3d
Alexis Abda <alexis.abda@hotmail.com>
2024-01-09 17:52:18 +0100

    not finish, better logic

--- CPP-06/ex00/ScalarConverter.cpp
+++ CPP-06/ex00/ScalarConverter.cpp
@@ -14,34 +14,54 @@ const char*	ScalarConverter::InvalidConvertException::what() const throw() {
 }
 
 void	ScalarConverter::print(char input) {
-	std::cout << "char: " << "'" << input << "'" << std::endl;
+	if (!isascii(static_cast<int>(input)))
+		std::cout << "char: " << "Impossible" << std::endl;
+	else if (isascii(static_cast<int>(input)) && !isprint(static_cast<int>(input)))
+		std::cout << "char: " << "Non displayable" << std::endl;
+	else
+		std::cout << "char: " << "'" << static_cast<char>(input) << "'" << std::endl;
 	std::cout << "int: " << static_cast<int>(input) << std::endl;
 	std::cout << "float: " << static_cast<float>(input) << ".0f" << std::endl;
 	std::cout << "double: " << static_cast<double>(input) << ".0" << std::endl;
 }
 
 void	ScalarConverter::print(int input) {
-	std::cout << "char: " << "'" << static_cast<char>(input) << "'" << std::endl;
+	if (!isascii(static_cast<int>(input)))
+		std::cout << "char: " << "Impossible" << std::endl;
+	else if (isascii(static_cast<int>(input)) && !isprint(static_cast<int>(input)))
+		std::cout << "char: " << "Non displayable" << std::endl;
+	else
+		std::cout << "char: " << "'" << static_cast<char>(input) << "'" << std::endl;
 	std::cout << "int: " << input << std::endl;
 	std::cout << "float: " << static_cast<float>(input) << ".0f" << std::endl;
 	std::cout << "double: " << static_cast<double>(input) << ".0" << std::endl;
 }
 
 void	ScalarConverter::print(float input) {
-	std::cout << "char: " << "'" << static_cast<char>(input) << "'" << std::endl;
+	if (!isascii(static_cast<int>(input)))
+		std::cout << "char: " << "Impossible" << std::endl;
+	else if (isascii(static_cast<int>(input)) && !isprint(static_cast<int>(input)))
+		std::cout << "char: " << "Non displayable" << std::endl;
+	else
+		std::cout << "char: " << "'" << static_cast<char>(input) << "'" << std::endl;
 	std::cout << "int: " << static_cast<int>(input) << std::endl;
 	std::cout << "float: " << input << "f" << std::endl;
 	std::cout << "double: " << static_cast<double>(input) << std::endl;
 }
 
 void	ScalarConverter::print(double input) {
-	std::cout << "char: " << "'" << static_cast<char>(input) << "'" << std::endl;
+	if (!isascii(static_cast<int>(input)))
+		std::cout << "char: " << "Impossible" << std::endl;
+	else if (isascii(static_cast<int>(input)) && !isprint(static_cast<int>(input)))
+		std::cout << "char: " << "Non displayable" << std::endl;
+	else
+		std::cout << "char: " << "'" << static_cast<char>(input) << "'" << std::endl;
 	std::cout << "int: " << static_cast<int>(input) << std::endl;
 	std::cout << "float: " << static_cast<float>(input) << "f" << std::endl;
 	std::cout << "double: " << input << std::endl;
 }
 
-void	ScalarConverter::parseInput(const std::string& input, int& sign, int& printable, int& dot, int& f, int& i_precision) {
+void	ScalarConverter::parseInputAndFillInfos(const std::string& input, int& sign, int& printable, int& dot, int& f, int& i_precision) {
 	int	digit = 0, ch = 0;
 
 	for (int i = 0; input[i]; ++i) {
@@ -60,44 +80,83 @@ void	ScalarConverter::parseInput(const std::string& input, int& sign, int& print
 		else if (isalpha(input[i]) && input[i] != 'f')
 			++ch;
 	}
-	std::cout
-	<<	"[input.length()] = " << input.length() << std::endl
-	<< "[dot] = " << dot << std::endl
-	<< "[sign] = " << sign << std::endl
-	<< "[f] = " << f << std::endl
-	<< "[printable] = " << printable << std::endl
-	<< "[i_precision] = " << i_precision << std::endl << std::endl;
 	if ((digit && ch) || dot > 1 || sign > 1 || f > 1 || printable > 1 
 		|| (f == 1 && input[input.length() - 1] != 'f') 
 		|| (sign == 1 && input[0] != '-'))
 		throw InvalidConvertException();
 }
 
-void	ScalarConverter::processInput(const std::string& input) {
-	int sign = 0, printable = 0, dot = 0, f = 0, i_precision = 0, totalAfterDot = 0;
+int	ScalarConverter::valueType(const std::string input, int dot, int f) {
 
-	parseInput(input, sign, printable, dot, f, i_precision);
-	if (i_precision)
-		totalAfterDot = input.length() - (i_precision + 1);
-	std::cout << totalAfterDot << std::endl;
-	if (!dot && !f) {
-		int	value_int = std::stoi(input);
-		print(value_int);
-	} else if (dot && f) {
-		float value_float = std::stof(input);
-		print(value_float);
-	} else if (dot && !f) {
-		double value_double = std::stod(input);
-		print(value_double);
+	if (!dot && f)																// fail
+		return e_fail;
+	else if (input.length() == 1 && isprint(input[0]) && !isdigit(input[0]))	// char
+		return e_char;
+	else if ((!dot && !f) || (input.length() == 1 && isdigit(input[0])))		// int
+		return e_int;
+	else if (dot && f)															// float
+		return e_float;
+	else if (dot && !f)															// double
+		return e_double;
+	return e_fail;
+}
+
+int	ScalarConverter::processInput(const std::string& input, std::stringstream& ss) {
+	int sign = 0, printable = 0, dot = 0, f = 0, i_precision = 0, type = 0;
+
+	parseInputAndFillInfos(input, sign, printable, dot, f, i_precision);
+	type = valueType(input, dot, f);
+	if (type == e_float) {
+		std::string			inputFloat(input.substr(0, input.length() - 1));
+		ss.str(inputFloat);
+	}
+	else
+		ss.str(input);
+	return type;
+}
+
+void	ScalarConverter::execute(std::stringstream& ss, int type) {
+	switch (type) {
+		case e_char:
+			char	valueChar;
+			ss >> valueChar;
+			if (ss.fail())
+				throw std::runtime_error("Impossible to convert string to char !");
+			print(valueChar);
+		break;
+		case e_int:
+			int	valueInt;
+			ss >> valueInt;
+			if (ss.fail())
+				throw std::runtime_error("Impossible to convert string to int !");
+			print(valueInt);
+		break;
+		case e_float:
+			float	valueFloat;
+			ss >> valueFloat;
+			if (ss.fail())
+				throw std::runtime_error("Impossible to convert string to float !");
+			print(valueFloat);
+		break;
+		case e_double:
+			double	valueDouble;
+			ss >> valueDouble;
+			if (ss.fail())
+				throw std::runtime_error("Impossible to convert string to double !");
+			print(valueDouble);
+		break;
+		default:
+			throw InvalidConvertException();
 	}
 }
 
 void	ScalarConverter::convert(std::string toBeConverted) {
 	try {
-		if (toBeConverted.length() == 1)
-			print(toBeConverted[0]);
-		else
-			processInput(toBeConverted);
+		std::stringstream	ss;
+		int					type = -1;
+
+		type = processInput(toBeConverted, ss);
+		execute(ss, type);
 	}
 	catch (std::exception& e) {
 		std::cerr << "[" << toBeConverted << "] " << e.what() << std::endl;
--- CPP-06/ex00/ScalarConverter.hpp
+++ CPP-06/ex00/ScalarConverter.hpp
@@ -2,9 +2,20 @@
 #define	SCALARCONVERTER_HPP
 
 #include <iostream>
+#include <sstream>
+#include <limits>
+#include <string>
+#include <stdexcept>
 
 class ScalarConverter {
 	private:
+		enum	types {
+			e_fail = -1,
+			e_char,
+			e_int,
+			e_float,
+			e_double
+		};
 		class InvalidConvertException : public std::exception {
 			public:
 				const char* what() const throw();
@@ -16,8 +27,10 @@ class ScalarConverter {
 		static void	print(int input);
 		static void	print(float input);
 		static void	print(double input);
-		static void	processInput(const std::string& input);
-		static void	parseInput(const std::string& input, int& sign, int& printable, int& dot, int& f, int& i_precision);
+		static void	parseInputAndFillInfos(const std::string& input, int& sign, int& printable, int& dot, int& f, int& i_precision);
+		static int	processInput(const std::string& input, std::stringstream& ss);
+		static int	valueType(const std::string input, int dot, int f);
+		static void	execute(std::stringstream& ss, int type);
 
 	public:
 		~ScalarConverter(void);
